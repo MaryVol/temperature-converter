d792e8f85d913d96770033e494fbf8c12b8e372d
MaryVol <meryswhite@icloud.com>
2020-04-10 21:17:58 +0300

    temp

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ let cel;
 let far;
 function cToF(c) {
  cel=(c*9/5)+32
- return(cel)
+ return cel.toFixed(2)
 }
 cToF(84);
 
 function fToC(f) {
  far=(f-32)*5/9
- return(far)
+ return far.toFixed(2)
 }
 fToC(67);
 
