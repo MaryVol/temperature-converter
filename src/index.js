190bd31b86623cbdeadcae92acf657137c208b37
MaryVol <54916409+MaryVol@users.noreply.github.com>
2020-04-10 18:16:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+'use strict'
 
-
-exports.cToF = function (c) {
-
+let cel;
+let far;
+function cToF(c) {
+ cel=(c*9/5)+32
+ console.log('cels to far', cel.toFixed(2))
 }
+cToF(84);
 
-exports.fToC = function (f) {
-
+function fToC(f) {
+ far=(f-32)*5/9
+ console.log('far to cels', far.toFixed(2))
 }
+fToC(67);
