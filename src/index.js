aab3d6a057e7dc7aa9c9da46c9d3f13317ed609c
MaryVol <meryswhite@icloud.com>
2020-04-10 20:05:04 +0300

    temperature

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-'use strict'
 
 let cel;
 let far;
 function cToF(c) {
  cel=(c*9/5)+32
- console.log('cels to far', cel.toFixed(2))
+ return(cel)
 }
 cToF(84);
 
 function fToC(f) {
  far=(f-32)*5/9
- console.log('far to cels', far.toFixed(2))
+ return(far)
 }
 fToC(67);
+
+exports.cToF = function (c) {
+
+}
+
+exports.fToC = function (f) {
+
+}
